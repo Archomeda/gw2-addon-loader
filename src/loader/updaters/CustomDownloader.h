7fdea7bb9d281dc13114fd1eb7796506bb377f75
Archomeda <archomeda@hotmail.com>
2018-08-23 00:32:34 +0200

    Fix 32-bit build

--- src/loader/updaters/CustomDownloader.h
+++ src/loader/updaters/CustomDownloader.h
@@ -13,7 +13,7 @@ namespace loader::updaters {
 
     protected:
         virtual void DownloadUpdate() override;
-        static void WriteBuffer(void* downloader, size_t progress, size_t totalSize);
+        static void GW2ADDON_CALL WriteBuffer(void* downloader, size_t progress, size_t totalSize);
 
     private:
         GW2AddonDownloadUpdate_t* downloadFunc;
